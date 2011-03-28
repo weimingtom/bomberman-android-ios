8ac271208cc6e65a7a5975442ff9fd3f2366bd29
benjamintardieu <benjamintardieu@108143a1-c41c-4495-23d9-9b134ce08c3b>
2011-03-28 17:53:07 +0000

    "Importing BomberKlob"

---
+++ trunk/dev/iOS/BomberKlob/BomberKlob/BomberKlobAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BomberKlobAppDelegate.h
+//  BomberKlob
+//
+//  Created by Benjamin Tardieu on 28/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BomberKlobAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ trunk/dev/iOS/BomberKlob/BomberKlob/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BomberKlob
+//
+//  Created by Benjamin Tardieu on 28/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
