9d1ea01c7102160cb9b4124e0643c1595136c134
olivier.bonvila <olivier.bonvila@108143a1-c41c-4495-23d9-9b134ce08c3b>
2011-04-12 16:29:55 +0000

    Android : Ajout des écouteurs(+ thread) pour les mouvements du joueurs

--- dev/Android/src/com/klob/Bomberklob/engine/GameController.java
+++ dev/Android/src/com/klob/Bomberklob/engine/GameController.java
@@ -1,19 +1,25 @@
 package com.klob.Bomberklob.engine;
 
+import com.klob.Bomberklob.objects.PlayerAnimations;
 import com.klob.Bomberklob.resourcesmanager.ResourcesManager;
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public abstract class GameController extends SurfaceView implements SurfaceHolder.Callback {
-	
+
 	protected GameView gameView;
-	
+
 	protected int objectsSize;
-	
+
+	protected int x;
+	protected int y;
+	protected PlayerAnimations animation = PlayerAnimations.IDLE;
+
 	/* Constructeurs  ------------------------------------------------------ */
 
 	public GameController(Context context) {		
@@ -21,24 +27,24 @@ public abstract class GameController extends SurfaceView implements SurfaceHolde
 		this.gameView = new GameView(getHolder(), this);
 		getHolder().addCallback(this);
 	}
-	
-    /* Getteurs ------------------------------------------------------------ */
-	
+
+	/* Getteurs ------------------------------------------------------------ */
+
 	public GameView getGameView() {
 		return gameView;
 	}
 
-    /* Setteurs ------------------------------------------------------------ */
-	
+	/* Setteurs ------------------------------------------------------------ */
+
 	public void setGameView(GameView gameView) {
 		this.gameView = gameView;
 	}
-	
+
 	/* Méthodes publiques -------------------------------------------------- */
 
 	@Override
 	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-		
+
 	}
 
 	@Override
@@ -66,7 +72,52 @@ public abstract class GameController extends SurfaceView implements SurfaceHolde
 		}
 		Log.i("GameController", "Thread done");		
 	}
-	
+
 	public abstract void onDraw(Canvas canvas, int size);
+
+
+	//FIXME Diagonales
+	@Override
+	public boolean onTouchEvent(MotionEvent event){
+		
+		switch (event.getAction()) {		
+		case MotionEvent.ACTION_DOWN:
+			this.x = (int) event.getX();
+			this.y = (int) event.getY();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			int xx = Math.abs(((int) event.getX()-this.x)/4);
+			int yy = Math.abs(((int) event.getY()-this.y)/4);
+			
+			if ( (int) event.getX() > this.x && ((int) event.getY() <  (this.y+xx)) && ((int) event.getY() > (this.y-xx)) ) {
+				animation = PlayerAnimations.RIGHT;
+			}
+			else if ( (int) event.getX() < this.x && ((int) event.getY() <  (this.y+xx)) && ((int) event.getY() > (this.y-xx)) ) {
+				animation = PlayerAnimations.LEFT;
+			}
+			else if ( (int) event.getY() < this.y && ((int) event.getX() <  (this.x+yy)) && ((int) event.getX() > (this.x-yy)) ) {
+				animation = PlayerAnimations.UP;
+			}
+			else if ( (int) event.getY() > this.y && ((int) event.getX() <  (this.x+yy)) && ((int) event.getX() > (this.x-yy)) ) {
+				animation = PlayerAnimations.DOWN;
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			if ( animation == PlayerAnimations.RIGHT) {
+				animation = PlayerAnimations.STOP_RIGHT;
+			}
+			else if ( animation == PlayerAnimations.LEFT) {
+				animation = PlayerAnimations.STOP_LEFT;
+			}
+			else if ( animation == PlayerAnimations.UP) {
+				animation = PlayerAnimations.STOP_UP;
+			}
+			else if ( animation == PlayerAnimations.DOWN) {
+				animation = PlayerAnimations.STOP_DOWN;
+			}
+			break;
+		}
+		return false;		
+	}
 
 }
--- dev/Android/src/com/klob/Bomberklob/engine/GameControllerSingle.java
+++ dev/Android/src/com/klob/Bomberklob/engine/GameControllerSingle.java
@@ -1,7 +1,12 @@
 package com.klob.Bomberklob.engine;
 
+import com.klob.Bomberklob.objects.PlayerAnimations;
+import com.klob.Bomberklob.resourcesmanager.ResourcesManager;
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.widget.FrameLayout;
 
@@ -9,12 +14,15 @@ public class GameControllerSingle extends GameController {
 	
 	private Engine engine;
 
+	private Thread onTouchEventThread;
+	private boolean onTouchEventBoolean;
 	
 	/* Constructeurs  ------------------------------------------------------ */
 	
 	public GameControllerSingle(Context context, String mapName, int enemies, String gametype, boolean random, int difficulty) {
 		super(context);
 		this.engine = new Engine(mapName, enemies, gametype, random, difficulty);
+		this.onTouchEventBoolean = true;
 	}
 	
 	/* Getteurs ------------------------------------------------------------ */
@@ -34,15 +42,51 @@ public class GameControllerSingle extends GameController {
 	public void surfaceCreated(SurfaceHolder arg0) {
 		super.surfaceCreated(arg0);
 		this.setLayoutParams(new FrameLayout.LayoutParams(this.engine.getSingle().getMap().getBlocks().length*this.objectsSize, this.engine.getSingle().getMap().getBlocks()[0].length*this.objectsSize));
+		this.onTouchEventThread = new Thread() {
+			@Override
+			public void run() {
+				while (onTouchEventBoolean) {
+					if ( animation == PlayerAnimations.RIGHT) {
+						Log.i("GameControllerSingle","DROITE");
+					}
+					else if ( animation == PlayerAnimations.LEFT) {
+						Log.i("GameControllerSingle","GAUCHE");
+					}
+					else if ( animation == PlayerAnimations.UP) {
+						Log.i("GameControllerSingle","HAUT");
+					}
+					else if ( animation == PlayerAnimations.DOWN) {
+						Log.i("GameControllerSingle","BAS");
+					}
+					else {
+						Log.i("GameControllerSingle",animation.getLabel());
+					}
+					try {
+						sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				Log.i("GameControllerSingle","Thread done");
+			};
+		};
+		this.onTouchEventThread.start();
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder arg0) {
 		super.surfaceDestroyed(arg0);
+		this.onTouchEventBoolean = false;
 	}
 
 	@Override
 	public void onDraw(Canvas canvas, int size) {
 		this.engine.onDraw(canvas,size);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event){
+		super.onTouchEvent(event);
+		return true;
 	}
 }
