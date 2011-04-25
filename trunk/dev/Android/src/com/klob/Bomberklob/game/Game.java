c91f387210d65c683caa1877338c4e230ab17b50
olivier.bonvila <olivier.bonvila@108143a1-c41c-4495-23d9-9b134ce08c3b>
2011-04-25 21:30:49 +0000

    Android : Optimisation de l'affichage de la carte

--- trunk/dev/Android/src/com/klob/Bomberklob/game/Game.java
+++ trunk/dev/Android/src/com/klob/Bomberklob/game/Game.java
@@ -1,34 +1,67 @@
 package com.klob.Bomberklob.game;
 
+import java.util.ArrayList;
+
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
+import com.klob.Bomberklob.objects.Destructible;
+import com.klob.Bomberklob.objects.Objects;
+import com.klob.Bomberklob.objects.ObjectsAnimations;
 import com.klob.Bomberklob.objects.Player;
+import com.klob.Bomberklob.resources.Point;
+import com.klob.Bomberklob.resources.ResourcesManager;
 
 public abstract class Game {
-	
+
 	protected Map map;
 	protected Player[] players;
 	protected GameType gameType;
-	
+
 	protected boolean random;
-	
+
+	Bitmap bm;
+	ArrayList<Objects> blocks;
+
 	/* Constructeurs  ------------------------------------------------------ */
-	
+
 	public Game(String mapName, int enemies, String gametype, boolean random) {
 		this.map = new Map();
 		if ( this.map.loadMap(mapName) ) {
 			this.players = new Player[enemies+1];
 			this.random = random;
-			
-			if ( gametype.equals("FreeForAll") ) {
-				this.gameType = new FreeForAll();
+			this.blocks = new ArrayList<Objects>();
+
+			bm = Bitmap.createBitmap(ResourcesManager.getSize()*this.map.getGrounds().length, ResourcesManager.getSize()*this.map.getGrounds()[0].length, Bitmap.Config.ARGB_8888);
+
+			Canvas pictureCanvas = new Canvas(bm);
+
+			//FIXME prendre le cas où on aurait de l'eau ou un sol animé !
+			this.map.groundsOnDraw(pictureCanvas, ResourcesManager.getSize());
+
+			for (int i = 0; i < this.map.getGrounds().length ; i++) {
+				for (int j = 0; j < this.map.getGrounds()[0].length ; j++) {
+					if ( this.map.getBlocks()[i][j] != null) {
+						if ( !this.map.getBlocks()[i][j].isDestructible()) {
+							this.map.getBlocks()[i][j].onDraw(pictureCanvas, ResourcesManager.getSize());
+						}
+						else {
+							blocks.add(this.map.getBlocks()[i][j]);
+						}
+					}
+				}
 			}
-			else {
-				
-			}			
 		}
+
+		if ( gametype.equals("FreeForAll") ) {
+			this.gameType = new FreeForAll();
+		}
+		else {
+
+		}			
 	}
-	
+
+
 	/* Getters ------------------------------------------------------------- */
 
 	public Map getMap() {
@@ -47,23 +80,40 @@ public abstract class Game {
 		return random;
 	}
 	
+	public ArrayList<Objects> getBlocks() {
+		return blocks;
+	}
+
 	/* Méthodes publiques -------------------------------------------------- */
-	
+
 	public abstract void initGame();
-	
+
 	public abstract void pushBomb(Player player);
 
 	public void onDraw(Canvas canvas, int size) {
-		this.map.onDraw(canvas, size);
+		canvas.drawBitmap(bm, 0, 0, null);
+
+		for (int i = 0 ; i < blocks.size() ; i++ ) {
+			blocks.get(i).onDraw(canvas, size);
+		}
+
 		for (int i = 0 ; i < this.players.length ; i++ ) {
 			if ( this.players[i].getPosition() != null ) {
 				this.players[i].onDraw(canvas, size);
 			}
 		}
 	}
-	
-	public void update() {
-		this.map.update();
-	}
 
+	public void update() {
+		
+		for (int i = 0 ; i < blocks.size() ; i++ ) {
+			if (blocks.get(i).getCurrentAnimation().equals(ObjectsAnimations.DESTROY.getLabel()) && blocks.get(i).hasAnimationFinished()) {
+				this.map.deleteBlock(new Point(blocks.get(i).getPosition().x/ResourcesManager.getSize(),blocks.get(i).getPosition().y/ResourcesManager.getSize()));
+				blocks.remove(i);
+			}
+			else {
+				blocks.get(i).update();
+			}
+		}
+	}
 }
