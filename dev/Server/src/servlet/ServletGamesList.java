1416368085b83fd7dbe1c2e94c5578cb0dc15a45
ludovicpitiot <ludovicpitiot@108143a1-c41c-4495-23d9-9b134ce08c3b>
2011-05-18 09:46:45 +0000

    nouvelle structure avec fichier cachés pour eclipse. Une simple importation du dossier est normalement possible maintenant. Ne pas oublier de créer une base de donnée locale nommée Bomberklob et rendre vos servlet accessible sur le port :8181 ou alors vous modifiez dans le code pour vos tests. Cette version a été déployé sur un serveur web et les requetes en accès via navigateur (GET) sont bonnes, mais l'appli android ne semble pas réussir à contacter ces servlets, problème de port sûrement.

---
+++ dev/Server/src/servlet/ServletGamesList.java
@@ -0,0 +1,73 @@
+package servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import flexjson.JSONSerializer;
+import game.Engines;
+
+/**
+ * Servlet implementation class ServletGamesList
+ */
+public class ServletGamesList extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletGamesList() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<h1>Servlet listant les parties en cour</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BufferedReader req=new BufferedReader(new InputStreamReader(request.getInputStream()));
+		OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream());
+			String message =req.readLine();
+			System.out.println("gl => " + message);
+			if(message.length() > 0){
+				
+				// vérification que l'utilisateur s'est connecté
+//				HashMap<String, String> users = (HashMap<String, String>) getServletContext().getAttribute("usersOnline");
+//				if(users.containsKey(message)){
+									
+					System.out.println(message);
+					
+					response.setContentType("text/html");
+					
+					JSONSerializer jsonSerializer = new JSONSerializer();				
+					Engines engines = (Engines) getServletContext().getAttribute("engines");
+	          	  	jsonSerializer.serialize(engines.gamesList(),writer);
+	          	  	writer.flush();
+//				}
+//				else{
+//					System.out.println(":: "+ message.length());
+//					writer.write("ERROR");
+//					writer.flush();
+//				}
+			}
+	}
+
+}
