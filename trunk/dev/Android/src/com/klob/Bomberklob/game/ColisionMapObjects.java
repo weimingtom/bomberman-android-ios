f8c0ad0c5202ccff83270a3497807f13c8e3f227
olivier.bonvila <olivier.bonvila@108143a1-c41c-4495-23d9-9b134ce08c3b>
2011-05-31 22:00:34 +0000

    RApport

--- trunk/dev/Android/src/com/klob/Bomberklob/game/ColisionMapObjects.java
+++ trunk/dev/Android/src/com/klob/Bomberklob/game/ColisionMapObjects.java
@@ -10,10 +10,25 @@ public enum ColisionMapObjects {
 	 * An area containing a block
 	 */
 	BLOCK(1),
+	/**
+	 * An area containing a gape
+	 */
 	GAPE(2),
+	/**
+	 * A damage zone
+	 */
 	DAMAGE(3),
+	/**
+	 * A dangerous area
+	 */
 	DANGEROUS_AREA(4),
+	/**
+	 * An area containing a bomb
+	 */
 	BOMB(5),
+	/**
+	 * An area containing fire
+	 */
 	FIRE(6);
 	
 	protected int label;
