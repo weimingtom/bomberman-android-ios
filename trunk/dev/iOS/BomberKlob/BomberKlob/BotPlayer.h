8af23b662d374470c467c7c8a3cc27fc277fbd92
benjamintardieu <benjamintardieu@108143a1-c41c-4495-23d9-9b134ce08c3b>
2011-05-31 21:09:50 +0000

    iOS: Documentation de la classe BotPlayer.

--- trunk/dev/iOS/BomberKlob/BomberKlob/BotPlayer.h
+++ trunk/dev/iOS/BomberKlob/BomberKlob/BotPlayer.h
@@ -3,6 +3,7 @@
 
 @class ColisionMap, Node;
 
+/** The `BotPlayer`class aims to manage a bot player. */
 @interface BotPlayer : Player {
     
 	NSString *difficulty;
@@ -18,21 +19,91 @@
     NSArray *enemies;
 }
 
+/** The map containing all informations for the bots (the untraversable case, the traversable case, the dangerous area, ect) */
 @property (nonatomic, retain) ColisionMap *colisionMap;
+
+/** The movement who bot must be do. */
 @property (nonatomic, retain) NSString *movement;
+
+/** If it must be plant a bomb. */
 @property (nonatomic, assign) BOOL plantingBomb;
+
+/** The enemis list. */
 @property (nonatomic, retain) NSArray *enemies;
+
+/** The path that bot must be travel. */
 @property (nonatomic, retain) NSArray *path;
 
+
+///--------------------------------------
+/// @name Initializing a BotPlayer Object
+///--------------------------------------
+
+/** Initializes and returns a newly allocated `BotPlayer` object with the specified position and colision map.
+ 
+ @param imageNameValue  
+ @param positionValue The bot position.
+ @param colisionMapValue The map containing all informations for the bots
+ @return A newly allocated `BotPlayer` object with the specified position and colision map.
+ */
 - (id)initWithImageName:(NSString *)imageNameValue position:(Position *)positionValue colisionMap:(ColisionMap *)colisionMapValue;
 
+
+///-------------------
+/// @name Computing A*
+///-------------------
+
+/** Computes the path with A* algorithm.
+ 
+ @param arrivedPosition 
+ @return An array containing the result of A* algorithm.
+ */
 - (NSArray *)pathFinding:(Position *)arrivedPosition;
+
+/** Updates the open list according to a new nodes list.
+ 
+ @param openList The open list.
+ @param closedList The close list.
+ @param newNodes The new nodes list.
+ */
 - (void)updateOpenList:(NSMutableDictionary *)openList closedList:(NSMutableDictionary *)closedList newNodes:(NSArray *)newNodes;
+
+/** Finds the best node in open list.
+ 
+ @param openList The open list.
+ @return The best node in open list.
+ */
 - (Node *)findBestNode:(NSDictionary *)openList;
+
+/** Computes the path with the closed list.
+ 
+ @param closedList The close list.
+ @param arrived The aim of player.
+ @return The path with the closed list.
+ */
 - (NSArray *)computePath:(NSDictionary *)closedList arrived:(Position *)arrived;
+
+
+///-------------------
+/// @name Managing Bot
+///-------------------
+
+/** Computes the direction according to the path. */
 - (void)computeDicrection;
+
+/** Makes action for the bot. */
 - (void)makeAction;
+
+/** Computes the path lenght. 
+ 
+ @return The path lenght.
+ */
 - (NSInteger)computeLenghtPath:(NSArray *)pathValue;
+
+/** Finds the nearest enemy. 
+ 
+ @return The nearest player.
+ */
 - (Player *)findNearestEnemy;
 
 @end
